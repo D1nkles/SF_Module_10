7f396af7034548551ef12cf5209bbeb00c64dd2c
Ilia <isinetov@mail.ru>
2024-05-06 20:38:20 +0400

    Задание 10.3.1. Создайте класс FileManager и выполните в нём множественную реализацию интерфейсов, указанных в примере выше.

---
+++ Task 10_3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿interface IWriter 
+{
+    void Write();
+}
+
+interface IReader 
+{
+    void Read();
+}
+
+interface IMailer 
+{
+    void SendMail();
+}
+
+class FileManager : IWriter, IReader, IMailer
+{
+    public void Write() 
+    {
+
+    }
+
+    public void Read() 
+    {
+
+    }
+
+    public void SendMail() 
+    {
+
+    }
+}
