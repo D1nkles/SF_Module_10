d47eb486c6d13be01c297bfc27ecd006a14f60d9
Ilia <isinetov@mail.ru>
2024-05-06 20:40:14 +0400

    Задание 10.3.2.

---
+++ Task 10_3_2/Task 10_3_2.cs
@@ -0,0 +1,32 @@
+﻿public interface ICreatable
+{
+    void Create();
+}
+
+public interface IDeletable
+{
+    void Delete();
+}
+
+public interface IUpdatable
+{
+    void Update();
+}
+
+class Entity : ICreatable, IDeletable, IUpdatable
+{
+    public void Create()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
