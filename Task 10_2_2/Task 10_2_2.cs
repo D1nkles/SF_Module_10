082f05403047593c1c0742cb28142674fc1c81eb
Ilia <isinetov@mail.ru>
2024-05-06 20:16:49 +0400

    Задание 10.2.2, Используя теоретический материал из данного юнита, постарайтесь самостоятельно реализовать явную реализацию следующего интерфейса:

---
+++ Task 10_2_2/Task 10_2_2.cs
@@ -0,0 +1,21 @@
+﻿public interface IWriter
+{
+    void Write();
+}
+
+class Writer : IWriter
+{
+    void IWriter.Write()
+    {
+        Console.WriteLine("Пишу!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        var writer = new Writer();
+        ((IWriter)writer).Write();
+    }
+}
