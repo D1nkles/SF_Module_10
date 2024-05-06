c96589e10d78665e31c93ab439faf755f7c82dff
Ilia <isinetov@mail.ru>
2024-05-06 19:46:35 +0400

    Задание 10.1.4. Класс Manager реализует интерфейс IManager. Попытайтесь самостоятельно определить интерфейс IManager, исходя из данного кода.

---
+++ Task 10_1_4/Task 10_1_4.cs
@@ -0,0 +1,31 @@
+﻿public interface IManager 
+{
+    void Create();
+    void Read();
+    void Update();
+    void Delete();
+
+}
+
+public class Manager : IManager
+{
+    public void Create()
+    {
+
+    }
+
+    public void Read()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+}
