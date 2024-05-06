38da4d10c3ca58b50d4206d3c33a9b6a90815ae1
Ilia <isinetov@mail.ru>
2024-05-06 20:49:39 +0400

    Задание 10.3.3.

---
+++ Task 10_3_3/Task 10_3_3.cs
@@ -0,0 +1,28 @@
+﻿public interface IBook
+{
+    void Read();
+}
+
+public interface IDevice
+{
+    void TurnOn();
+    void TurnOff();
+}
+
+class ElectronicBook : IBook, IDevice
+{
+    void IBook.Read()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IDevice.TurnOff()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IDevice.TurnOn()
+    {
+        throw new NotImplementedException();
+    }
+}
