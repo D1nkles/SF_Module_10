0c358a2e6474114397626d34f44336a41812c55f
Ilia <isinetov@mail.ru>
2024-05-06 21:20:23 +0400

    Задание 10.4.4,

---
+++ Task 10_4_4/Program.cs
@@ -0,0 +1,32 @@
+﻿public class User
+{
+
+}
+
+public class Account : User
+{
+
+}
+
+class UserService : IUpdater<User>
+{
+    public void Update(User entity)
+    {
+        Console.WriteLine("Обновили!");
+    }
+}
+
+public interface IUpdater<in T>
+{
+    void Update(T entity);
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        IUpdater<Account> updater = new UserService();
+        updater.Update(new Account());
+    }
+    
+}
